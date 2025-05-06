b9d497b41516e087349f624348acd75da12da8cb
Hussain Khokhar <hussainkhokhar800@gmail.com>
2025-05-06 11:21:11 +0500

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./components/Maincomp"
+import Navbar from "./components/Navbar"
+import "./App.css";
+
+export default function App() {
+    return (
+       <>
+       <Navbar />
+       <Main />
+       </>
+    )
+}
